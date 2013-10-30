d9df8dd0b43340c5dffb76f904a6095d1be1a2a1
dhacker29 <davidhackerdvm@gmail.com>
2013-10-29 22:59:55 -0500

    Remove cdma.mk and gsm.mk
    Change-Id: Ic5bb1364ebcb02d9b37c6fb58278f4fe3a2143be

--- cm.mk
+++ cm.mk
@@ -2,11 +2,7 @@
 $(call inherit-product, device/motorola/solana/full_solana.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
 
 #
 # Setup device specific product configuration.
