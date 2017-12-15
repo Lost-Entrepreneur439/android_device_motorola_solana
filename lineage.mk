f2715543a8373f3ea6ca5c8487a214ef17a3926e
Michael Gernoth <michael@gernoth.net>
2017-12-15 09:56:17 +0100

    solana: depend on mini_phone instead of full_phone
    frees up some space on /system
    
    Change-Id: Ie6dd5c66f5487c3d51b8b3dffb85b001ef2495bd

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/motorola/solana/full_solana.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 #
 # Setup device specific product configuration.
